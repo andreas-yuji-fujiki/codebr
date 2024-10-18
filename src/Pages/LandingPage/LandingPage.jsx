ae2344bdee0b415a8251660841b4875346a5e6f2
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2024-10-18 10:01:34 -0300

    renamed AppHeader folder

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,5 @@
 
-import { AppHeader } from './Components/Header/AppHeader'
+import { AppHeader } from './Components/AppHeader/AppHeader'
 import HeroSection from './Components/HeroSection/HeroSection'
 export function LandingPage(){
   return(
