a0d5d77937bc20f5d5c0cfad76c32ea34fe4ce82
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2024-10-22 19:52:13 -0300

    Updates

--- assets/index-Bzu8ZUpT.js
+++ assets/index-Bzu8ZUpT.js
@@ -413,7 +413,7 @@ Error generating stack: `+l.message+`
         padding: 135px 0 70px 0;
     }
     @media (max-width: 450px){
-        padding: 125px 0 90px 0;
+        padding: 125px 0 82px 0;
     }
 
     .stars1 {
