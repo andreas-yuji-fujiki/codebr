c07b06dcc7badc7e3c66caeb69b3379b22714ea1
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2024-10-22 19:49:22 -0300

    changed hero section padding top/bottom

--- src/Pages/LandingPage/Components/HeroSection/HeroSection.styled.js
+++ src/Pages/LandingPage/Components/HeroSection/HeroSection.styled.js
@@ -21,7 +21,7 @@ export const MainSection = styled.section`
         padding: 135px 0 70px 0;
     }
     @media (max-width: 450px){
-        padding: 115px 0 100px 0;
+        padding: 125px 0 96px 0;
     }
 
     .stars1 {
