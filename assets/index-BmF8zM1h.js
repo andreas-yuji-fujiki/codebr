7057024025dc4d747680c27274be37a883f1e59d
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2024-10-22 20:03:41 -0300

    Updates

--- assets/index-BmF8zM1h.js
+++ assets/index-BmF8zM1h.js
@@ -413,7 +413,7 @@ Error generating stack: `+l.message+`
         padding: 135px 0 70px 0;
     }
     @media (max-width: 450px){
-        padding: 125px 0 93px 0;
+        padding: 122px 0 93px 0;
     }
 
     .stars1 {
