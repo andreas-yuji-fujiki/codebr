521bfded1673a1cf934ba6d57425969c066b166d
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2024-10-22 19:49:32 -0300

    Updates

--- assets/index-UZ3l9nJn.js
+++ assets/index-UZ3l9nJn.js
@@ -413,7 +413,7 @@ Error generating stack: `+l.message+`
         padding: 135px 0 70px 0;
     }
     @media (max-width: 450px){
-        padding: 115px 0 100px 0;
+        padding: 125px 0 96px 0;
     }
 
     .stars1 {
