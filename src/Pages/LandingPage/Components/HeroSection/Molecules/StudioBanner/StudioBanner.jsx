c8d502f1861081b6cbc150f0ab1d089acfcf63e4
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2024-10-24 22:27:00 -0300

    centered vertically the components on banner section

--- src/Pages/LandingPage/Components/HeroSection/Molecules/StudioBanner/StudioBanner.jsx
+++ src/Pages/LandingPage/Components/HeroSection/Molecules/StudioBanner/StudioBanner.jsx
@@ -18,6 +18,7 @@ import styled from "styled-components"
 const BannerContainer = styled.div `
     display: flex;
     justify-content: center;
+    align-items: center;
 
     img{
         position: relative;
