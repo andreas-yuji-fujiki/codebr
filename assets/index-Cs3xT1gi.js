f298c1b9ea1f128c1296fb98d3932e95cac01fab
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2024-10-25 15:53:34 -0300

    Updates

--- assets/index-Cs3xT1gi.js
+++ assets/index-Cs3xT1gi.js
@@ -659,6 +659,10 @@ Error generating stack: `+o.message+`
     @media (max-width: 768px) {
       width: 100%;
       margin-top: 10%;
+
+      .cardItems{
+        font-size: 12px;
+      }
     }
 
     // general styles
