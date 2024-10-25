5fb4c7a23d9ddbaf5289b2e3fbe67744747b065e
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2024-10-25 11:42:04 -0300

    app header now turns bg color to black when scrolling

--- src/Pages/LandingPage/Components/AppHeader/AppHeader.jsx
+++ src/Pages/LandingPage/Components/AppHeader/AppHeader.jsx
@@ -1,20 +1,36 @@
+import { useState, useEffect } from "react";
+
 // components
-import { LogoTitle } from "./Atoms/LogoTitle/LogoTitle"
-import { MobileHeaderNav } from "./Molecules/HeaderNav/MobileHeaderNav/MobileHeaderNav"
-import { HeaderNav } from "./Molecules/HeaderNav/HeaderNav"
-import { AccountSection } from "./Molecules/AccountSection/AccountSection"
+import { LogoTitle } from "./Atoms/LogoTitle/LogoTitle";
+import { MobileHeaderNav } from "./Molecules/HeaderNav/MobileHeaderNav/MobileHeaderNav";
+import { HeaderNav } from "./Molecules/HeaderNav/HeaderNav";
+import { AccountSection } from "./Molecules/AccountSection/AccountSection";
 
 // styles
-import { AppHeaderContainer, AppHeaderBox } from "./AppHeader.styled"
+import { AppHeaderContainer, AppHeaderBox } from "./AppHeader.styled";
+
 export const AppHeader = () => {
-    return(
-        <AppHeaderBox>
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 0);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <AppHeaderBox isScrolled={isScrolled}>
             <AppHeaderContainer>
-                <MobileHeaderNav/>
-                <LogoTitle/>
-                <HeaderNav/>
-                <AccountSection/>
+                <MobileHeaderNav />
+                <LogoTitle />
+                <HeaderNav />
+                <AccountSection />
             </AppHeaderContainer>
         </AppHeaderBox>
-    )
-}
+    );
+};
--- src/Pages/LandingPage/Components/AppHeader/AppHeader.styled.js
+++ src/Pages/LandingPage/Components/AppHeader/AppHeader.styled.js
@@ -1,25 +1,24 @@
 import styled from "styled-components";
 
 export const AppHeaderBox = styled.header`
-    /* background-color: #0e1015; */
+    background-color: ${({ isScrolled }) => (isScrolled ? "#000" : "transparent")};
     z-index: 1900;
-    padding: 27px 10px 20px 10px;
+    padding: 27px 10px 8px 10px;
     width: 100%;
-
     position: fixed;
     top: 0;
     left: 0;
-`
+    transition: background-color 0.3s ease; /* Transição suave */
+`;
 
 export const AppHeaderContainer = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-
     max-width: 1440px;
     margin: 0 auto;
 
-    @media (max-width: 1525px){
+    @media (max-width: 1525px) {
         max-width: 95vw;
     }
-`
+`;
