928f1b91cb51b8c8bd6169ee4b3858ba97a6fc48
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2024-11-16 12:44:18 -0300

    changed the main title

--- src/Pages/LandingPage/Components/HeroSection/Atoms/Title/CallToActionTitle.jsx
+++ src/Pages/LandingPage/Components/HeroSection/Atoms/Title/CallToActionTitle.jsx
@@ -3,6 +3,6 @@ import { CallToActionTitleContainer } from "../Title/CallToActionTitle.styled";
 export const  CallToActionTitle = () => (
     <CallToActionTitleContainer>
         Construa seu projeto <br className="mobile"/> em <br className="desktop"/>
-        <span className="mobile-decoration">três cliques.</span> <span className="desktop">Não é exagero!</span>
+        <span className="mobile-decoration">30 segundos.</span> <span className="desktop">Não é exagero!</span>
     </CallToActionTitleContainer>
 );
